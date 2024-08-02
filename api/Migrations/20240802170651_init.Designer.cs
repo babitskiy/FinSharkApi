a3f60f874b737b666e41f09d9235f0ce00563a68
evgenijbabickij <babickii_@mail.ru>
2024-08-02 22:00:05 +0300

    feat: Add Dtos and mappers

--- api/Migrations/20240802170651_init.Designer.cs
+++ api/Migrations/20240802170651_init.Designer.cs
@@ -12,7 +12,7 @@ using api.Data;
 namespace api.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    [Migration("20240731132849_init")]
+    [Migration("20240802170651_init")]
     partial class init
     {
         /// <inheritdoc />
