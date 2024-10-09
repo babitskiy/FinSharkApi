8a47ec00f2aa3ec0248cb32694433267642f35f8
Бабицкий Евгений <e.babitskiy@datareon.ru>
2024-10-09 21:04:56 +0300

    feat: implement stocks sorting

--- api/Repositories/StockRepository.cs
+++ api/Repositories/StockRepository.cs
@@ -49,6 +49,16 @@ namespace api.Repositories
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDecsending 
+                        ? stocks.OrderByDescending(s => s.Symbol)
+                        : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
